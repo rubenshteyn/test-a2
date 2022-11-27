1cbc57217b51b8617161c0906e4dbee495766e58
ilia_rubenshtein <vasilijpupkin6101@gmail.com>
2022-11-28 01:29:28 +0300

    test-a2

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <UserSettings/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import UserSettings from './components/UserSettings.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    UserSettings
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import vSelect from 'vue-select'
+import Popper from "vue3-popper"
+import "vue-select/dist/vue-select.css";
 
-createApp(App).mount('#app')
+
+createApp(App)
+    .component('v-select', vSelect)
+    .use(Popper)
+    .mount('#app')
